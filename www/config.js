608ff0b4a2f5b418d7831a66977f03f443a1e2f2
Tianling Shen <cnsztl@immortalwrt.org>
2023-10-27 14:54:08 +0800

    www: rename to ImmortalWrt
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- www/config.js
+++ www/config.js
@@ -5,21 +5,22 @@ var config = {
   show_help: true,
 
   // Versions list (optional if provided by .versions.json)
-  versions: ["23.05.4", "19.07.10"],
+  // versions: ["23.05.4", "21.02.7"],
 
   // Pre-selected version (optional if provided by .versions.json)
-  default_version: "23.05.4",
+  // default_version: "23.05.4",
 
   // Image download URL (e.g. "https://downloads.openwrt.org")
-  image_url: "../misc",
+  image_url: "https://downloads.immortalwrt.org",
 
   // Insert snapshot versions (optional)
-  //show_snapshots: true,
+  show_snapshots: true,
 
   // Info link URL (optional)
   info_url: "https://openwrt.org/start?do=search&id=toh&q={title} @toh",
 
   // Attended Sysupgrade Server support (optional)
-  asu_url: "https://sysupgrade.openwrt.org",
-  asu_extra_packages: ["luci"],
+  // asu_url: "https://sysupgrade.immortalwrt.org",
+  asu_url: "https://sysupgrade.kyarucloud.moe",
+  // asu_extra_packages: [ "luci" ],
 };
